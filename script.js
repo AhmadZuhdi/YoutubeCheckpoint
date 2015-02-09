75da4f9c68065de4f55bc94fc7855b3b2a3ce7b4
AhmadZuhdi <rccheattest2@gmail.com>
2015-02-09 14:53:24 +0700

    v0.2, add more information on localStorage object

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         Youtube Checkpoint
 // @namespace    https://github.com/AhmadZuhdi/YoutubeCheckpoint
-// @version      0.1
+// @version      0.2
 // @description  Resume youtube from last time visit
 // @author       AhmadZuhdi
-// @require		 http://code.jquery.com/jquery-2.1.3.min.js
-// @require 	 https://raw.github.com/lodash/lodash-compat/3.1.0/lodash.min.js
+// @require      http://code.jquery.com/jquery-2.1.3.min.js
+// @require      https://raw.github.com/lodash/lodash-compat/3.1.0/lodash.min.js
 // @require      http://momentjs.com/downloads/moment.min.js
+// @require      https://raw.githubusercontent.com/madrobby/keymaster/master/keymaster.js
 // @match        https://www.youtube.com/watch*
 // @grant        none
 // ==/UserScript==
@@ -36,7 +37,7 @@
     app.start = function() {
      
         app.youtube = document.getElementById("movie_player");
-      	
+        
         if(!localStorage.getItem('ytcp')) {
          
             console.log('cannot get localstorage')
@@ -56,6 +57,8 @@
         app.checkCurrentCheckPoint()
         
         app.createCloseListener()
+        
+        app.createKeyboardListener()
         
     }
     
@@ -77,32 +80,68 @@
     
     app.checkCurrentCheckPoint = function() {
         
-        if(app.data[app.get.v]){
+        if(app.data[app.get.v]){            
             
-         	window.location.hash = 't=' +  app.data[app.get.v]
+            window.location.hash = 't=' +  app.data[app.get.v].time
             
         }
         
+    }
+    
+    app.createKeyboardListener = function() {
+        
+        try {
+         
+            if(key){
+         
+                key('command+k ctrl+k', function(e){
+                
+                    app.getHistory()
+                
+                })
+                
+            } else {
+             
+                console.log('Key is not defined')
+                
+            }
+            
+        } catch (e) {
+            
+            console.log(e)
+            
+        }
+        
+    }
+    
+    app.getHistory = function() {
+                    
+        console.log(app.data)
+
     }
     
     app.createCloseListener = function () {
         
         window.onbeforeunload = function() {
             
-			app.data[app.get.v] = app.youtube.getCurrentTime()
+            app.data[app.get.v] = {}
             
-            console.log(app.data)
-
-            console.log($('eow-title').text())
-
-         	// localStorage.setItem('ytcp', JSON.stringify(app.data))
+            var checkpoint = app.data[app.get.v]
             
-            return 'test'
-            	
+            checkpoint.time = app.youtube.getCurrentTime()
+            
+            checkpoint.title = $('#eow-title').text().trim()
+            
+            checkpoint.date = moment(new Date()).format('YYYY-MM-DD HH:mm:ss')
+
+            localStorage.setItem('ytcp', JSON.stringify(app.data))
+            
+            // return 'test'
+                
         }
         
     }
     
     app.init()
-	
+    
 })()
